30d441eca49be59d17108fb61858f51b1ec4be57
Michael Petrovich <michael.c.petrovich@gmail.com>
2026-08-23 03:26:44 +0200

    feat(site): rebrand homepage as Petro.blog
    Use Petro.blog as the site title and drop the repeated name from the intro.

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,8 @@
 export default {
-	title: "Michael Petrovich",
-	description:
-		"Writing at the intersection of product, engineering, and data.",
-	url: "https://mpetrovich.github.io/Blog",
-	author: "Michael Petrovich",
-	linkedin: "https://www.linkedin.com/in/mpetrovich/",
-	github: "https://github.com/mpetrovich/",
-};
+    title: 'Petro.blog',
+    description: 'Writing at the intersection of product, engineering, and data.',
+    url: 'https://mpetrovich.github.io/Blog',
+    author: 'Michael Petrovich',
+    linkedin: 'https://www.linkedin.com/in/mpetrovich/',
+    github: 'https://github.com/mpetrovich/',
+}
