8d5e78a9cef407619a09924da22249b65f65a4a8
Michael Petrovich <michael.c.petrovich@gmail.com>
2026-08-24 22:36:25 +0200

    Add Plausible analytics to site head.

---
+++ site/src/_data/analytics.js
@@ -0,0 +1,5 @@
+export default `<script async src="https://plausible.io/js/pa-epO2nIry4x_UDvcncqrjK.js"></script>
+<script>
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+</script>`
